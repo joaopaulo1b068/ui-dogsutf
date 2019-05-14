312aa1511733fb65c9df394f5bf30688d93ec038
joaopaulo1b068 <joaopaulo0696@yahoo.com.br>
2019-05-14 20:34:38 -0300

    add dog img to banner
    font: https://webcachorros.com.br/adotar-um-cao-de-rua/

--- src/libs/urls.js
+++ src/libs/urls.js
@@ -1,2 +1,3 @@
 export const LOGO_HEADER = 'https://res.cloudinary.com/hqbf9dy02/image/upload/v1557864182/dogsutf/235405_supeej.png'
 export const BAR_ICON_HEADER = 'https://res.cloudinary.com/hqbf9dy02/image/upload/v1557865783/dogsutf/bars-icon_yo1ivc.png'
+export const IMG_BANNER = 'https://res.cloudinary.com/hqbf9dy02/image/upload/v1557876808/dogsutf/8b4a0f3dcdd598afa05b71009ad30f6c_ytcvq1.jpg'
