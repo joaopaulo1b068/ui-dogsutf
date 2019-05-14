0d2f116bf8388f63ff59349f40950cc95e1f1ec1
joaopaulo1b068 <joaopaulo0696@yahoo.com.br>
2019-05-14 20:11:18 -0300

    create url consts

---
+++ src/libs/urls.js
@@ -0,0 +1,2 @@
+export const LOGO_HEADER = 'https://res.cloudinary.com/hqbf9dy02/image/upload/v1557864182/dogsutf/235405_supeej.png'
+export const BAR_ICON_HEADER = 'https://res.cloudinary.com/hqbf9dy02/image/upload/v1557865783/dogsutf/bars-icon_yo1ivc.png'
