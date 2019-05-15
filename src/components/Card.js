00c27c494c5df7266976bd3aca1ca7fa2d981596
joaopaulo1b068 <joaopaulo0696@yahoo.com.br>
2019-05-15 16:13:18 -0300

    cria card
    fundo preto de exemplo

---
+++ src/components/Card.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import styled from 'styled-components'
+
+export const Card = () => {
+    return (
+        <StyledCard>
+            
+        </StyledCard>
+    )
+}
+
+const StyledCard = styled.div`
+    background-color: black;
+    height: 50px;
+`
