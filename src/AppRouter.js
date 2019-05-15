3695392a1d77d4d4f87ce9d4b7f4f71784bc34f0
joaopaulo1b068 <joaopaulo0696@yahoo.com.br>
2019-05-15 20:59:03 -0300

    cria container de rota com tela padrao (header footer)

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import {BrowserRouter, Route} from 'react-router-dom'
 import {HomeScreen} from './screens/HomeScreen'
+import styled from 'styled-components'
+import {Header} from './components/Header'
+import {Footer} from './components/Footer'
+import {GRAY} from './styles/colors'
+import {XLARGE} from './libs/grid'
 
 // const PublicRoute = ({component: Component, ...params}) => {
 //   <Route 
@@ -13,11 +18,29 @@ import {HomeScreen} from './screens/HomeScreen'
 //   />
 // }
 
+const PublicRoute = ({component: Component, ...params}) => (
+  <Route {...params} render={props =>  (
+      <StyledScreen>
+        <Header {...props} />
+        <Component {...props}  />
+        <Footer  />
+      </StyledScreen>) 
+  }/>
+)
+
+const StyledScreen = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  width: 100%;
+  background-color: ${GRAY};
+`
+
 export const AppRouter = () => {
   return (
 
     <BrowserRouter>
-      <Route path="/" exact component={HomeScreen} />
+      <PublicRoute path="/" exact component={HomeScreen} />
     </BrowserRouter>
   )
 }
