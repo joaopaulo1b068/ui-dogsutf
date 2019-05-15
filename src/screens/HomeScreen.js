bafaf03c450b3a0ac55bf01f88f0020f07a27368
joaopaulo1b068 <joaopaulo0696@yahoo.com.br>
2019-05-15 16:11:24 -0300

    use selector and list items on home screen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import { Header } from '../components/Header';
 import styled from 'styled-components'
+import { Selector } from '../components/Selector';
+import {Lista} from '../components/Lista'
 
 export const HomeScreen = (props) => {
 
     return (
         <StyledHomeScreen>
             <Header route={props.history.location.pathname} />
+            <Selector />
+            <Lista />
         </StyledHomeScreen>
     )
 }
