23b65559311ad9e909769eb128b486714479db91
joaopaulo1b068 <joaopaulo0696@yahoo.com.br>
2019-05-15 20:55:09 -0300

    centraliza home screen e coloca fundo branco

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,14 +1,13 @@
 import React from 'react'
-import { Header } from '../components/Header';
 import styled from 'styled-components'
 import { Selector } from '../components/Selector';
 import {Lista} from '../components/Lista'
+import {XLARGE} from '../libs/grid'
 
 export const HomeScreen = (props) => {
 
     return (
         <StyledHomeScreen>
-            <Header route={props.history.location.pathname} />
             <Selector />
             <Lista />
         </StyledHomeScreen>
@@ -16,5 +15,9 @@ export const HomeScreen = (props) => {
 }
 
 const StyledHomeScreen = styled.div`
-    background-color: rgba(211,211,211,0.2);
+    background-color: white;
+    max-width: ${XLARGE};
+    margin: 0 auto;
+    height: 100%;
+    width: 100%;
 `
