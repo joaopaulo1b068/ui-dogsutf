d68ab06b62ecab6e895e5035e6e9feba2503b8d4
joaopaulo1b068 <joaopaulo0696@yahoo.com.br>
2019-05-16 13:19:03 -0300

    atualiza card com sexo, tamanho e local

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,15 +1,36 @@
 import React from 'react'
 import styled from 'styled-components'
+import {PURPLE} from '../styles/colors'
 
-export const Card = () => {
+export const Card = (props) => {
     return (
         <StyledCard>
-            
+            <img src={props.dog.img} alt="doguinho"></img>
+            <span>{props.dog.sexo || 'sexo'}</span>
+            <span>{props.dog.tamanho || 'tamanho'}</span>
+            <span>{props.dog.cidade || 'cidade'} / {props.dog.uf || 'uf'}</span>
         </StyledCard>
     )
 }
 
 const StyledCard = styled.div`
-    background-color: black;
-    height: 50px;
+    background: rgba(255,255,0, 0.4);
+    border-radius: 4px;
+    padding: 10px;
+    display: flex;
+    flex-direction: column;
+
+    img{
+        height: 150px;
+        width: 100%;
+        display: block;
+        border-radius: 4px;
+    }
+
+    span{
+        color: black;
+        text-align: center;
+        margin-top: 10px;
+    }
+
 `
