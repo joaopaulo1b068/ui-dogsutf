e1664874d27240da4e184faf4d5403600f1205ae
joaopaulo1b068 <joaopaulo0696@yahoo.com.br>
2019-05-17 17:13:50 -0300

    cria reducer de cadastro

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -2,30 +2,41 @@ import { combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import { createStore } from 'redux'
 
-import { CLICK_UPDATE_VALUE } from './action_types';
+import { CLICK_UPDATE_VALUE, SIGNUP_SUCCESS, SIGNUP_ERROR } from './action_types';
 
 const initialState = {
-  newValue: 'nenhm',
-  thunk: 'fds',
+   newValue: 'nenhm',
+   thunk: 'fds',
 }
 
 const clickReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case CLICK_UPDATE_VALUE:
-      return { ...state, newValue: action.newValue }
-    case 'THUNK': 
-      console.log(action.payload)
-      return {...state, thunk: JSON.stringify(action.payload)}
-    case 'SIGNUP_SUCCESS':
-        console.log(action.payload)
-        return { ...state, user: action.payload }
-    default:
-      return state;
-  }
-};
+   switch (action.type) {
+      case CLICK_UPDATE_VALUE:
+         return { ...state, newValue: action.newValue }
+      case 'THUNK':
+         console.log(action.payload)
+         return { ...state, thunk: JSON.stringify(action.payload) }
+      default:
+         return state;
+   }
+}
+
+const signupReducer = (state = { user: false, error: false }, action) => {
+
+   let {type, payload} = action
+
+   switch (type){
+      case SIGNUP_SUCCESS:
+         return { ...state, user: payload }
+      case SIGNUP_ERROR:
+         return {...state, error: payload}
+      default: return state
+   }
+}
 
 const Reducers = combineReducers({
-  clickState: clickReducer
+   signupState: signupReducer,
+   clickState: clickReducer
 })
 
 export const Store = createStore(Reducers, applyMiddleware(thunk));
