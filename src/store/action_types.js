7a09cf7ec0b23e3a76999fcf5de8f60127dcde8b
joaopaulo1b068 <joaopaulo0696@yahoo.com.br>
2019-05-17 17:12:31 -0300

    cria actions do reducer de cadastro

--- src/store/action_types.js
+++ src/store/action_types.js
@@ -1,1 +1,7 @@
-export const CLICK_UPDATE_VALUE = 'CLICK_UPDATE_VALUE';
+export const CLICK_UPDATE_VALUE = 'CLICK_UPDATE_VALUE'
+
+/**
+ * signup reducer
+ */
+export const SIGNUP_SUCCESS = 'SIGNUP_SUCCESS'
+export const SIGNUP_ERROR = 'SIGNUP_ERROR'
