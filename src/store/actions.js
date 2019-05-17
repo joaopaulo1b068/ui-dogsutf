f47aae5cf40573c74ebb0945ca5a248e19335b06
joaopaulo1b068 <joaopaulo0696@yahoo.com.br>
2019-05-17 09:50:30 -0300

    setup do thunk
    fonte: https://github.com/reduxjs/redux-thunk

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,6 +1,28 @@
-import { CLICK_UPDATE_VALUE } from './action_types';
+import { CLICK_UPDATE_VALUE } from './action_types'
+import axios from 'axios'
 
-export const clickButton = value => ({
-    type: CLICK_UPDATE_VALUE,
-    newValue: value
-  });
+export const clickButton = (value) => ({
+  type: CLICK_UPDATE_VALUE,
+  newValue: value
+})
+
+const ajax = async () => {
+  return await axios.get('https://jsonplaceholder.typicode.com/todos/1')
+}
+
+const makeASandwich = (data) => {
+  return {
+    type: 'THUNK',
+    payload: data
+  }
+}
+
+export const asyncAct = (value) => {
+
+  return (dispatch) => {
+    return ajax().then(
+      sauce => dispatch(makeASandwich(sauce.data)),
+      //error => dispatch(apologize('The Sandwich Shop', forPerson, error))
+    )
+  }
+}
--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,19 +1,21 @@
-import { combineReducers } from 'redux';
+import { combineReducers, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
 import { createStore } from 'redux'
 
 import { CLICK_UPDATE_VALUE } from './action_types';
 
 const initialState = {
-  newValue: 'nenhm'
+  newValue: 'nenhm',
+  thunk: 'fds',
 }
 
 const clickReducer = (state = initialState, action) => {
   switch (action.type) {
     case CLICK_UPDATE_VALUE:
-      return {
-        ...state,
-        newValue: action.newValue
-      };
+      return { ...state, newValue: action.newValue }
+    case 'THUNK': 
+      console.log(action.payload)
+      return {...state, thunk: JSON.stringify(action.payload)}
     default:
       return state;
   }
@@ -23,4 +25,4 @@ const Reducers = combineReducers({
   clickState: clickReducer
 })
 
-export const Store = createStore(Reducers);
+export const Store = createStore(Reducers, applyMiddleware(thunk));
