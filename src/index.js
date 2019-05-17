52c44d019bcd09b3754a6a9c4f4508441c53e8c9
joaopaulo1b068 <joaopaulo0696@yahoo.com.br>
2019-05-17 09:08:39 -0300

    usa provider react dom render

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { AppRouter } from './AppRouter';
+import { Store } from './store/reducers';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<AppRouter />, document.getElementById('root'))
+
+ReactDOM.render(
+    <Provider store={Store}>
+        <AppRouter />
+    </Provider>, document.getElementById('root'))
